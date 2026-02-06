b09de175def783a3a4986dfa071c2a241ff7c4de
Zi Tao Li <dylanlzt2003@gmail.com>
2026-02-05 23:48:57 -0800

    Embed fr3_manipulation instead of submodule

---
+++ fr3_manipulation/source/fr3_manipulation/docs/CHANGELOG.rst
@@ -0,0 +1,10 @@
+Changelog
+---------
+
+0.1.0 (2025-12-08)
+~~~~~~~~~~~~~~~~~~
+
+Added
+^^^^^
+
+* Created an initial template for building an extension or project based on Isaac Lab
---
+++ fr3_manipulation/source/fr3_manipulation/fr3_manipulation/tasks/manager_based/fr3_manipulation/robot/franka3_contact_cfg.py
@@ -0,0 +1,56 @@
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+ASSET_DIR = os.path.join(BASE_DIR, "..", "assets")  
+FR3_USD_PATH = os.path.join(ASSET_DIR, "franka3.usd")
+
+FRANKA_3_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=str(FR3_USD_PATH),
+        activate_contact_sensors=False,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=0
+        ),
+        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "fr3_joint1": 0.0,
+            "fr3_joint2": -0.569,
+            "fr3_joint3": 0.0,
+            "fr3_joint4": -2.810,
+            "fr3_joint5": 0.0,
+            "fr3_joint6": 3.037,
+            "fr3_joint7": 0.741,
+            "fr3_finger_joint.*": 0.04,
+        },
+    ),
+    actuators={
+        "fr3_shoulder": ImplicitActuatorCfg(
+            joint_names_expr=["fr3_joint[1-4]"],
+            effort_limit_sim=87.0,
+            stiffness=80.0,
+            damping=4.0,
+        ),
+        "fr3_forearm": ImplicitActuatorCfg(
+            joint_names_expr=["fr3_joint[5-7]"],
+            effort_limit_sim=12.0,
+            stiffness=80.0,
+            damping=4.0,
+        ),
+        "fr3_hand": ImplicitActuatorCfg(
+            joint_names_expr=["fr3_finger_joint.*"],
+            effort_limit_sim=200.0,
+            stiffness=2e3,
+            damping=1e2,
+        ),
+    },
+    soft_joint_pos_limit_factor=1.0,
+)
